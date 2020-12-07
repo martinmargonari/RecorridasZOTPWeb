534b946ecd01a6f9334d8ac410a57dc8976e071f
Martin Margonari <martinmargonari@gmail.com>
2020-12-06 21:11:17 -0300

    Added validations to events

--- spec/features/eventos/creating_evento_spec.rb
+++ spec/features/eventos/creating_evento_spec.rb
@@ -6,7 +6,27 @@ RSpec.feature "Crear evento" do
   let!(:persona) { create(:person) }
   let(:evento) { build(:evento )}
 
-  scenario "siendo administrador" do
+  scenario "satisfactoriamente" do
+    login_as admin
+    visit "/"
+
+    click_link "Configuración"
+    click_link "Calendario"
+    click_link DateTime.now.day.to_s
+
+    fill_in "Título", with: evento.titulo
+    fill_in "Descripción", with: evento.descripcion
+
+    select_date_and_time evento.fecha_desde, from: "evento_fecha_desde"
+    select_date_and_time evento.fecha_hasta, from: "evento_fecha_hasta"
+    select persona.nombre, from: "Persona"
+    fill_in "Ubicación", with: evento.ubicacion
+    click_button "Aceptar"
+
+    expect(page).to have_content("Evento creado correctamente")
+  end
+
+  scenario "satisfactoriamente" do
     login_as admin
     visit "/"
 
